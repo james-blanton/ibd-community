4689463cc267e0af7e0e9dd2a595bab3f0a15614
James Blanton <charlesjamesblanton@gmail.com>
2018-05-11 19:39:19 -0400

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -5,13 +5,6 @@ $db_username= "root";
 $db_password= "";
 $dbname= "ibd-community";
 
-/*
-$dbServername= "localhost";
-$db_username= "chblanton";
-$db_password= "J!anie11841";
-$dbname= "ibd_community";
-*/
-
 $conn = mysqli_connect($dbServername, $db_username, $db_password, $dbname);
 
 ?>
